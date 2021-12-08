defbfe7b7f2bfbb7abe35638f41506fea5a747c1
SeherSaylik <sehersaylik@users.noreply.github.com>
2021-12-08 22:13:03 +0300

    Updates

---
+++ precache-manifest.5266dcbb6ee52824c666083339664300.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdde27a117a212f6a743",
+    "url": "/cartoons-nft-marketplace/static/css/main.e3b90a55.chunk.css"
+  },
+  {
+    "revision": "bdde27a117a212f6a743",
+    "url": "/cartoons-nft-marketplace/static/js/main.bdde27a1.chunk.js"
+  },
+  {
+    "revision": "78ae297d8128234e75c6",
+    "url": "/cartoons-nft-marketplace/static/js/1.78ae297d.chunk.js"
+  },
+  {
+    "revision": "6d62890a1aa03b059f4d",
+    "url": "/cartoons-nft-marketplace/static/js/runtime~main.6d62890a.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/cartoons-nft-marketplace/static/media/loading.26ef8bb4.gif"
+  },
+  {
+    "revision": "f78293fe1a3f7ae70d50674fcdfbcf70",
+    "url": "/cartoons-nft-marketplace/static/media/metamask.f78293fe.svg"
+  },
+  {
+    "revision": "82873c78f5ada5c05b1fe8afebc23dbe",
+    "url": "/cartoons-nft-marketplace/index.html"
+  }
+];
